b75ac6b3add2a41263a296edc7d96c0fd0d15bf2
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-07-06 13:48:47 +0900

    Last Person to Fit in the Bus - 마저 풀어야 함

--- LeetCode/1204.sql
+++ LeetCode/1204.sql
@@ -12,4 +12,5 @@ FROM
     )
 WHERE TOTAL_WEIGHT <= 1000
 
+-- 1000을 초과하기 직전까지의 테이블은 구했는데 거기서 가장 최대값인 존시나를 어떻게 뽑아내야할지 모르겠음 ㅜㅜ 
 -- 문제 출처: https://leetcode.com/problems/last-person-to-fit-in-the-bus/description/

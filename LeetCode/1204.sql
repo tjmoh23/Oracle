10e724cd40e97abb7fde13487eb08074121d044f
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-07-19 20:37:26 +0900

    Last Person to Fit in the Bus

--- LeetCode/1204.sql
+++ LeetCode/1204.sql
@@ -1,16 +1,20 @@
-SELECT *
-FROM
-    (
-        SELECT
-        TURN, 
-        PERSON_ID,
-        PERSON_NAME,
-        WEIGHT,
-        SUM(WEIGHT) OVER(ORDER BY TURN
-                        ROWS UNBOUNDED PRECEDING) AS TOTAL_WEIGHT
-    FROM QUEUE
-    )
-WHERE TOTAL_WEIGHT <= 1000;
+SELECT PERSON_NAME
+FROM(
+    SELECT 
+        PERSON_NAME, SUM,
+        RANK() OVER(ORDER BY SUM DESC) AS RANK
+    FROM
+        (
+        SELECT 
+            PERSON_NAME, WEIGHT, TURN,
+            SUM(WEIGHT) OVER(ORDER BY TURN
+                            ROWS UNBOUNDED PRECEDING) AS SUM
+        FROM QUEUE
+        ORDER BY TURN
+        )
+    WHERE SUM <= 1000)
+WHERE RANK = 1
 
--- 1000을 초과하기 직전까지의 테이블은 구했는데 거기서 가장 최대값인 존시나를 어떻게 뽑아내야할지 모르겠음 ㅜㅜ 
+-- 1000을 초과하기 직전까지의 테이블은 구했는데 거기서 가장 최대값인 존시나를 어떻게 뽑아내야할지 어려웠음 ㅜㅜ 
+-- 윈도우 함수 사용해서 RANK컬럼 만들고 인라인 뷰로 한 번 더 만들어서 조건절로 뽑아냈다.
 -- 문제 출처: https://leetcode.com/problems/last-person-to-fit-in-the-bus/description/

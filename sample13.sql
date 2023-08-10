805b7c77a254c9adeb378d40d4af7ac700ebd61c
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-08-10 11:58:38 +0900

    프로그래머스 문제 풀이

---
+++ sample13.sql
@@ -0,0 +1,45 @@
+-- 직접 만드신 문제 1
+-- CAR_RENTAL_COMPANY_CAR 테이블과 CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 대여기간(rental_period)을 반영한 CAR_ID 별 대여요금(rental_period * DAILY_FEE) 누적합 리스트(컬럼명 : CUMUL_SUM)를 추가하고 
+-- CAR_ID, 대여기간, 일일 대여요금, 대여요금 누적합을 조회하는 SQL문을 작성해주세요.
+
+SELECT 
+    CAR_ID,
+    RENTAL_PERIOD,
+    DAILY_FEE,
+    SUM(RENTAL_PERIOD*DAILY_FEE) OVER(PARTITION BY CAR_ID
+                        ORDER BY HISTORY_ID
+                        ROWS UNBOUNDED PRECEDING) AS CUMUL_SUM
+FROM
+(
+    SELECT
+    H.HISTORY_ID,
+    H.CAR_ID,
+    H.END_DATE - H.START_DATE + 1 AS RENTAL_PERIOD,
+    C.DAILY_FEE
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY H, CAR_RENTAL_COMPANY_CAR C
+    WHERE H.CAR_ID = C.CAR_ID
+)
+
+
+-- 직접 만드신 문제 2
+-- CAR_RENTAL_COMPANY_CAR 테이블과 CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 고객이 가장 많이 렌트한 자동차 종류와 
+-- 해당 자동차 종류의 옵션 리스트 중에서 후방카메라가 포함된 자동차의 옵션 리스트를 조회하는 쿼리를 작성해주세요. 최종 결과는 중복된 값이 없도록 해주세요.
+
+SELECT *
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE CAR_TYPE IN (
+                    SELECT 
+                        CAR_TYPE,
+                        COUNT(HISTORY_ID) AS CNT
+                    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY H, CAR_RENTAL_COMPANY_CAR C
+                    WHERE H.CAR_ID = C.CAR_ID
+                    GROUP BY CAR_TYPE
+                    ORDER BY CNT DESC
+                    FETCH FIRST 1 ROWS ONLY )
+
+
+
+
+
+
+-- 문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/157341

4b99be990fab601fdcb3837d4d485542b7019a21
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-08-15 09:21:49 +0900

    프로그래머스 문제 풀이

--- sample13.sql
+++ sample13.sql
@@ -25,6 +25,8 @@ FROM
 -- CAR_RENTAL_COMPANY_CAR 테이블과 CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 고객이 가장 많이 렌트한 자동차 종류와 
 -- 해당 자동차 종류의 옵션 리스트 중에서 후방카메라가 포함된 자동차의 옵션 리스트를 조회하는 쿼리를 작성해주세요. 최종 결과는 중복된 값이 없도록 해주세요.
 
+
+-- 풀이 1)
 SELECT DISTINCT CAR_TYPE, OPTIONS
 FROM CAR_RENTAL_COMPANY_CAR
 WHERE CAR_TYPE IN (
@@ -37,6 +39,23 @@ WHERE CAR_TYPE IN (
                             WHERE H.CAR_ID = C.CAR_ID
                             GROUP BY CAR_TYPE )
                     WHERE RANK = 1 )
+AND OPTIONS LIKE '%후방카메라%'
+
+
+-- 풀이2) 상빈님 풀이
+SELECT DISTINCT CAR_TYPE, OPTIONS
+FROM CAR_RENTAL_COMPANY_CAR 
+WHERE CAR_TYPE = (
+    SELECT CAR_TYPE
+    FROM (
+        SELECT C.CAR_TYPE, COUNT(H.HISTORY_ID) AS CNT
+        FROM CAR_RENTAL_COMPANY_CAR C, CAR_RENTAL_COMPANY_RENTAL_HISTORY H
+        WHERE C.CAR_ID = H.CAR_ID
+        GROUP BY C.CAR_TYPE
+        ORDER BY CNT DESC
+        FETCH FIRST 1 ROWS ONLY
+    )
+)
 AND OPTIONS LIKE '%후방카메라%'
 
 

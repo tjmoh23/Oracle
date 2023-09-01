2407e733d7fa7c7af6406522f6b315a63d40afaf
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-09-01 23:17:56 +0900

    Advanced Select -- 다시 풀어보기(9/1)

---
+++ HackerRank/Occupations.sql
@@ -0,0 +1,19 @@
+-- 행과 열 바꾸는 PIVOT 활용
+-- 직업 아래에 이름이 표시되도록 직업 열을 피봇
+-- 알파벳순으로 정렬
+
+
+SELECT
+    MIN(CASE WHEN OCCUPATION = 'Doctor' THEN NAME ELSE NULL END),
+    MIN(CASE WHEN OCCUPATION = 'Professor' THEN NAME ELSE NULL END),
+    MIN(CASE WHEN OCCUPATION = 'Singer' THEN NAME ELSE NULL END),
+    MIN(CASE WHEN OCCUPATION = 'Actor' THEN NAME ELSE NULL END)
+FROM(
+        SELECT
+            OCCUPATION, NAME, 
+            ROW_NUMBER() OVER(PARTITION BY OCCUPATION
+                              ORDER BY NAME) AS NB
+        FROM OCCUPATIONS
+    )
+GROUP BY NB
+ORDER BY NB;

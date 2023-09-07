d8f7c858eba0ee6070e8137ab7cee704acc515f3
이호연 <51068987+tjmoh23@users.noreply.github.com>
2023-09-07 16:59:24 +0900

    MEDIAN( ) 중앙값 구하기 | 다른 방법 ?

---
+++ HackerRank/Weather_Observation_Station_20.sql
@@ -0,0 +1,12 @@
+-- 중앙값 구하기
+
+-- MEDIAN( ) 함수 사용하기
+SELECT
+    ROUND(MEDIAN(LAT_N), 4)
+FROM STATION;
+
+
+-- 함수 사용하지 않고는 어떻게 풀까?
+
+
+-- 문제 출처: https://www.hackerrank.com/challenges/weather-observation-station-20/problem?isFullScreen=true
